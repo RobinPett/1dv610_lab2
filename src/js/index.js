5ca68838566c7990c10d1c3fe30e2b61d965665d
rp222nc <rp222nc@student.lnu.se>
2024-09-30 13:22:54 +0200

    Document public functions

--- src/js/index.js
+++ src/js/index.js
@@ -4,77 +4,101 @@
 import { ColorPaletteFromPixels } from "./ColorPaletteFromPixels.js"
 import { ImageToPixels } from "./ImageToPixels.js"
 
-let imageURL = "https://i.ibb.co/syRwkSk/Cirkel.png"
-
-imageURL = 'https://cdn.konst.se/konstverk/800/2501830840652.jpg'
-
+// Test Images --------------------------------------------------------------------
+let imageURL = 'https://cdn.konst.se/konstverk/800/2501830840652.jpg'
 // imageURL = 'https://i.ibb.co/FhZVb9q/test.jpg' // Red green and blue test image
-
 // imageURL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg' // Black image
-
-// imageURL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Puesta_de_sol%2C_Tok%2C_Alaska%2C_Estados_Unidos%2C_2017-08-28%2C_DD_189-191_HDR.jpg/640px-Puesta_de_sol%2C_Tok%2C_Alaska%2C_Estados_Unidos%2C_2017-08-28%2C_DD_189-191_HDR.jpg'
-
-// imageURL = 'https://i.ibb.co/9rDyJzT/color-Test.jpg'
-
-// imageURL = 'https://i.ibb.co/8NSZWDf/small-tets-image.jpg'
-
-
+// imageURL = 'https://i.ibb.co/8NSZWDf/small-tets-image.jpg' // Small colorful image of houses
+// imageURL = 'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg' // Cors image test
 // ----------------------------------------------------------------------------------------
 
 console.log('Color Palette Extractor connected to browser')
 
-// const body = document.querySelector('body')
-// const img = document.createElement('img')
-// img.src = imageURL
-// img.style.width = 500
-
-// body.appendChild(img)
-
-// extract rgba values from image
-
 /**
- * Load an image and create
+ * 
  * @param {string} imageUrl - URL to image
  * @returns
  */
-export function extractPixelsFromImage(imageUrl) {
-    const imageToPixels = new ImageToPixels(imageURL)
-    return imageToPixels
+export function loadImage(imageUrl) {
+    return new ImageToPixels(imageURL)
 }
 
-export async function getRgbaValues(imageToPixels) {
-    const rgbaValues = await imageToPixels.getRgbaValues()
-    return rgbaValues
-}
-
+/**
+ * Extracts dominant colors from pixel data representing an image:
+ * [ { red, green, blue, alpha } ]
+ * 
+ * @param {Array} rgbaValues - Array of pixel data with rgba values. 
+ * @param {number} numberOfColorsToExtract - How many colors in palette
+ * @returns {object} - Extracted colors
+ */
 export function getColorPalette(rgbaValues, numberOfColorsToExtract) {
     const colorPaletteFromPixels = new ColorPaletteFromPixels(rgbaValues, numberOfColorsToExtract)
     const extractedColors = colorPaletteFromPixels.getDominantColors()
     return extractedColors
 }
 
-// Example usage
+/**
+ * Presents the color palette as colored divs.
+ * Ignoring alpha channel. 
+ * 
+ * @param {Array} colorPalette - Array of color palette objects: { red, green, blue, alpha }
+ * @param {number} size - Size of each square div in px. 
+ * @returns {HTMLDivElement} - Color Palette container div.
+ */
+export function presentColorPalette(colorPalette, size) {
+    size ? size = size : 100
+    
+    const containerDiv = document.createElement('div')
+    containerDiv.style.display = 'flex'
+    containerDiv.style.flexDirection = 'row'
 
-const imageToPixels = extractPixelsFromImage(imageURL)
-console.log('Image to pixels: ')
-console.log(imageToPixels)
+    // Create colored divs
+    colorPalette.forEach((color) => {
+    const div = document.createElement('div')
+    div.style.backgroundColor = `rgb(${color.red}, ${color.green}, ${color.blue})`
+    div.style.height = `${size}px`
+    div.style.width = `${size}px`
 
-const pixels = await imageToPixels.getRgbaValues()
+    containerDiv.appendChild(div)
+    })
+
+    return containerDiv
+}
+
+
+
+// Example usage ------------------------------------------------------
+
+const image = loadImage(imageURL)
+
+const pixels = await image.getPixels()
+const imageHeight = await image.getHeightInPx()
+const imageWidth = await image.getWidthInPx()
+const imageElement = await image.getImageElement()
+
+const colorPalette = getColorPalette(pixels, 5)
+
+const colorPaletteDiv = presentColorPalette(colorPalette, 100)
+
+// Present in your own html document
+const body = document.querySelector('body')
+body.append(imageElement)
+body.append(colorPaletteDiv)
+
+// TODO ?
+// getMutedColorPalette()
+// getBrightColorPalette()
+
+
+// Test logs
+console.log('Loaded image: ')
+console.log(image)
+
+console.log('Image Height: ' + imageHeight)
+console.log('Image Width: ' + imageWidth)
+
 console.log('Pixels: ')
 console.log(pixels)
 
-const colorPalette = getColorPalette(pixels, 5)
 console.log('Color palette: ')
 console.log(colorPalette)
-
-
-
-// Create divs with color info extracted
-// extractedColors.forEach((color) => {
-//     const div = document.createElement('div')
-//     div.style.backgroundColor = `rgb(${color.red}, ${color.green}, ${color.blue})`
-//     div.style.height = '100px'
-//     div.style.width = '100px'
-
-//     body.appendChild(div)
-// })
